aa403d560f32ab9ff69b78e440837b9bb61b0402
GamerGoals Developer <ggdev@mrbadaxe.no-ip.org>
2013-02-14 23:36:18 -0500

    created stub index and login pages

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>GamerGoals</title>
+</head>
+<body>
+
+<h1>GamerGoals</h1>
+<div>under construction</div><br/>
+
+<a href="./login.php">Login</a><br/>
+<a href="./register.php">Register</a><br/>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,29 @@
+<?
+
+include_once './classes/AccountHandler.php';
+
+if($_POST['user'] && $_POST['pass']){
+	$vUser = $_POST['user'];
+	$vPass = $_POST['pass'];
+
+	if(AccountHandler->validateLogin($vUser,$vPass)){
+		/* create cookie */
+		/* redirect to home */
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login | GamerGoals</title>
+</head>
+<body>
+	<h1>Login</h1>
+	<form>
+	<label>Username</label>
+	<input name="user" type="text" /><br/>
+	<label>Password</label>
+	<input name="pass" type="password" /><br/>
+	<input name="submit" type="submit" />
+	</form>
+</body>
+</html>
