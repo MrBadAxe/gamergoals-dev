73d2765c3cbe38cbf5133cb8f9bd2c8e5a87f362
GamerGoals Developer <ggdev@mrbadaxe.no-ip.org>
2013-07-13 23:50:39 -0400

    rudimentary listing of goals

--- viewgame.php
+++ viewgame.php
@@ -3,10 +3,12 @@
 include_once "./Game.php";
 include_once "./GameView.php";
 include_once "./GameHandler.php";
+include_once "./GoalHandler.php";
 include_once "./AccountHandler.php";
 use Gamergoals\Game;
 use Gamergoals\GameView;
 use Gamergoals\GameHandler;
+use Gamergoals\GoalHandler;
 use Gamergoals\AccountHandler;
 
 
@@ -57,17 +59,33 @@ use Gamergoals\AccountHandler;
 				if(!GameHandler::ownsGame($a, $g)){ ?>
 					<div class="span4 offset4"><?=GameView::generateStatusButton($g, $a)?></div>
 				<? }else{ ?>
-				Add Goal code here
-				<!-- 
-					show 'add new goal' button
-					get goal list
-					for each goal
-						show description
-						show button to modify description
-						if binary, put button to change status
-						if numeric, put fields to increment/decrement number
-					show another 'add new goal' button
-				 -->
+					<div class="span4 offset4">
+						<a class="btn btn-large" href="./addgoal.php">Add New Goal</a>
+					</div>
+					
+					<div class="span10 offset1">
+					<?
+						$list = new GoalList($a);
+
+						$nlist = $list->getGoalsForGame($g);
+						foreach($nlist as $ng){
+							/* 
+							show description
+							show button to modify description
+							if binary, put button to change status
+							if numeric, put fields to increment/decrement number
+							*/
+							?>
+							<div class="well well-small">
+								<?=$ng->toCSVString()?>
+							</div>
+							<?
+						}
+					?>
+					</div>
+					<div class="span4 offset4">
+						<a class="btn btn-large" href="./addgoal.php">Add New Goal</a>
+					</div>
 				<? } ?>
 			<? } ?>
 			</div>
