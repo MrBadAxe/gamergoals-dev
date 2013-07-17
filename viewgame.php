1b31b0b671166658b717a951cfdaa3dece7af61d
GamerGoals Developer <ggdev@mrbadaxe.no-ip.org>
2013-07-17 00:56:38 -0400

    modified layout; things should now be nested properly

--- viewgame.php
+++ viewgame.php
@@ -2,11 +2,15 @@
 
 include_once "./Game.php";
 include_once "./GameView.php";
+include_once "./GoalList.php";
+include_once "./GoalListView.php";
 include_once "./GameHandler.php";
 include_once "./GoalHandler.php";
 include_once "./AccountHandler.php";
 use Gamergoals\Game;
 use Gamergoals\GameView;
+use Gamergoals\GoalList;
+use Gamergoals\GoalListView;
 use Gamergoals\GameHandler;
 use Gamergoals\GoalHandler;
 use Gamergoals\AccountHandler;
@@ -36,61 +40,57 @@ use Gamergoals\AccountHandler;
 
 <? include "./navbar.inc"; ?>
 <div class="container">
-	<div class="row text-center">
+	<div class="row-fluid text-center">
+	<? if($g == NULL){ ?>
 		<h2><?=$title?></h2>
-		<div class="">
-		<? if($g == NULL){ ?>
-			<a class="btn btn-large btn-primary" href="./search.php">Search for another Game</a>
-		<? }else{ ?>
+		<a class="btn btn-large btn-primary" href="./search.php">Search for another Game</a>
+	<? }else{ ?>
+		<h2><?=$title?></h2>
+		<div class="container-fluid">
 			<div class="row">
 				<div class="span6 offset3">for <i><?=GameView::formatPlatform($g->getPlatform())?></i> (<?=$g->getYear()?>)</div>
 			</div>	
 			<div class="row">
 				<div class="span6 offset3">
-				Developed by <?=$g->getDeveloper()?>&nbsp;&mdash;&nbsp;
-				Published by <?=$g->getPublisher()?></div>
+				Developed by <?=$g->getDeveloper()?>&nbsp;&mdash;&nbsp;Published by <?=$g->getPublisher()?></div>
 			</div>
-			</br>
-			<div>
-			<? if(!$_COOKIE['user']){ ?>
-				<a class="btn btn-large btn-standard" href="./login.php">Login to Add to Your Collection</a>
-			<? }else{
-				$a = AccountHandler::accountFromName($_COOKIE['user']);
-				if(!GameHandler::ownsGame($a, $g)){ ?>
+		</div>
+		</br>
+		<div class="container-fluid">
+		<? if(!$_COOKIE['user']){ ?>
+			<a class="btn btn-large btn-standard" href="./login.php">Login to Add to Your Collection</a>
+		<? }else{
+			$a = AccountHandler::accountFromName($_COOKIE['user']);
+			if(!GameHandler::ownsGame($a, $g)){ ?>
+				<div class="row-fluid">
 					<div class="span4 offset4"><?=GameView::generateStatusButton($g, $a)?></div>
-				<? }else{ ?>
+				</div>
+			<? }else{ ?>
+				<div class="row-fluid">
 					<div class="span4 offset4">
 						<a class="btn btn-large" href="./addgoal.php">Add New Goal</a>
 					</div>
-					
+				</div>
+				
+				<div class="row-fluid">
 					<div class="span10 offset1">
 					<?
 						$list = new GoalList($a);
-
-						$nlist = $list->getGoalsForGame($g);
-						foreach($nlist as $ng){
-							/* 
-							show description
-							show button to modify description
-							if binary, put button to change status
-							if numeric, put fields to increment/decrement number
-							*/
-							?>
-							<div class="well well-small">
-								<?=$ng->toCSVString()?>
-							</div>
-							<?
-						}
+						$glv = new GoalListView($list);
+						print $glv->displayGoalsForGame($g);
 					?>
 					</div>
+				</div>
+				
+				<div class="row-fluid">
 					<div class="span4 offset4">
 						<a class="btn btn-large" href="./addgoal.php">Add New Goal</a>
 					</div>
-				<? } ?>
+				</div>
 			<? } ?>
-			</div>
 		<? } ?>
 		</div>
+	<? } ?>
 	</div>
 </div>
 
